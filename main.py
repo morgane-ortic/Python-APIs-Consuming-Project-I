bd3116f20ba8fd9bd624cd5eeb83ad201c4724e8
Morgane Ortic <morgane.ortic@proton.me>
2024-11-12 11:34:23 +0100

    changed default item number to 15 for data retrieving

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from retrieve_data import DataHandler
 
 def main():
-    data_handler = DataHandler(item_number=3)
+    data_handler = DataHandler(item_number=15)
     data_handler.show_data()
 
 if __name__ == '__main__':
