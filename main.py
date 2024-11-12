a45f977b10f1538eebb398a7461d7c5105599b19
Morgane Ortic <morgane.ortic@proton.me>
2024-11-12 11:30:23 +0100

    Implemented pokemon printing from the retrieved data (if existing) or the json files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from retrieve_data import DataHandler
 
 def main():
     data_handler = DataHandler(item_number=3)
-    data_handler.get_data()
+    data_handler.show_data()
 
 if __name__ == '__main__':
     main()
--- retrieve_data.py
+++ retrieve_data.py
@@ -16,6 +16,10 @@ class DataHandler:
         self.get_places()
         self.save_data()
 
+    def show_data(self):
+        self.create_dicts()
+        self.print_data()
+
     def save_data(self):
         with open('pokemons.json', 'w') as f:
             json.dump(self.pokemons, f, indent=4)
@@ -23,8 +27,11 @@ class DataHandler:
             json.dump(self.images, f, indent=4)
         with open('places.json', 'w') as f:
             json.dump(self.places, f, indent=4)
-        
 
+    def read_json_file(self, data_type):
+        with open(f'{data_type}.json', 'r') as file:
+            return json.load(file)
+        
     def get_pokemons(self):
         for i in range(self.item_number):
             response = requests.get(f'{self.base_url}/custom?_quantity=1&customfield1=pokemon')
@@ -46,3 +53,44 @@ class DataHandler:
             latitude = response_data['data'][0].get('latitude')
             longitude = response_data['data'][0].get('longitude')
             self.places.append({f'latitude_{i+1}': latitude, f'longitude_{i+1}': longitude})
+
+    def create_dicts(self):
+
+        if not self.pokemons:
+            self.pokemons = self.read_json_file('pokemons')
+        if not self.images:
+            self.images = self.read_json_file('images')
+        if not self.places:
+            self.places = self.read_json_file('places')
+
+        pokemons_num = len(self.pokemons)
+        images_num = len(self.images)
+        places_num = len(self.places)
+        
+        # in case the lists have a different number of items,
+        # the script will leave aside the data that has no corresponding data in other lists
+        dict_num = min(pokemons_num, images_num, places_num)
+
+        self.poke_data = []
+        for i in range(dict_num):
+            pokelocation = {
+                'pokemon': self.pokemons[i][f'pokemon_{i+1}'],
+                'image': self.images[i][f'image_{i+1}'],
+                'location': {
+                    'latitude': self.places[i][f'latitude_{i+1}'],
+                    'longitude': self.places[i][f'longitude_{i+1}']
+                },
+            }
+            self.poke_data.append(pokelocation)
+
+
+    def print_data(self):
+        for i, pokemon in enumerate(self.poke_data, start=1):            
+            print('--------')
+            print(f'Pokemon {i}')
+            print('--------')
+            print(f'Name: {pokemon['pokemon']}')
+            print(f'Image: {pokemon['image']}')
+            print(f'Latitude: {pokemon['location']['latitude']}')
+            print(f'Longitude: {pokemon['location']['longitude']}')
+        print('--------')
